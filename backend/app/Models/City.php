beac1c81c712ab750e43e9a1bc3289d3d6468208
asagohan2301 <shemomechama@yahoo.co.jp>
2024-06-24 21:43:21 +0900

    都市名から緯度経度を取得する処理を作成

--- backend/app/Models/City.php
+++ backend/app/Models/City.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    use HasFactory;
+    protected $fillable = ['latitude', 'longitude'];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
